941d27022f7cb5df542c676c14c8f906add984e8
Michelle Thomas <karmadog@users.noreply.github.com>
2017-10-23 19:15:56 -0500

    Update LibraryApp.java
    Translate from Polish to English

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -3,7 +3,7 @@ package app;
 public class LibraryApp {
 
 	public static void main(String[] args) {
-		final String appName = "Bliblioteka v1.0";
+		final String appName = "Library v1.0";
 		System.out.println(appName);
 		LibraryControl libControl = new LibraryControl();
 		libControl.controlLoop();
